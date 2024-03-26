0edc113beebcb6f57b0b80f68cbc982cde2a6483
Denison Kolling <denisonkolling@gmail.com>
2024-03-26 08:56:52 -0300

    first commit

---
+++ src/main/java/com/pharmacymanage/PharmacyManageApplication.java
@@ -0,0 +1,13 @@
+package com.pharmacymanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyManageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pharmacymanage/PharmacyManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharmacymanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
